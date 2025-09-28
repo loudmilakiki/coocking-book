f8075b72ab7046ba901aea9ca24cd39a5310d8c4
loudmilakiki <loudahandmade@gmail.com>
2025-09-28 21:10:54 +0300

    without burger

--- js/scripts/burger.js
+++
@@ -1,6 +0,0 @@
-const burgerBtn = document.querySelector(".header__burger");
-const menu = document.querySelector(".header__menu");
-
-burgerBtn.addEventListener("click", () => {
-  menu.classList.toggle("show");
-});
